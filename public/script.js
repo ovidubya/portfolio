00bdb2454c12db43f25873da59bf22cdd4de861a
ovidubya <ovidubs@gmail.com>
2020-06-28 16:14:36 -0500

    added gtm

--- public/script.js
+++ public/script.js
@@ -3,41 +3,29 @@
   w.gaIdentifer =
     w.location.href.indexOf("qa") > -1 ||
     w.location.href.indexOf("localhost") > -1
-      ? "UA-170794679-1"
-      : "UA-170761872-1";
+      ? { ga: "UA-170794679-1", gtm: "GTM-MPXHPN4" }
+      : { ga: "UA-170761872-1", gtm: "GTM-NLV4BVR" };
   w.ga =
     w.ga ||
     function () {
       (ga.q = ga.q || []).push(arguments);
     };
   ga.l = +new Date();
-  ga("create", w.gaIdentifer, "auto");
+  ga("create", w.gaIdentifer.ga, "auto");
   ga("send", "pageview");
   w.gaa = function (category, action, label) {
     ga("send", "event", category, action, label);
   };
 })(window);
 
-(function (w) {
-  function sendToSlack(text) {
-    return fetch(
-      "https://hooks.slack.com/services/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",
-      {
-        method: "POST",
-        headers: {},
-        body: JSON.stringify({
-          text: text,
-        }),
-      }
-    )
-      .then((data) => data.text())
-      .then((result) => result);
-  }
-
-  w.addEventListener("load", function () {
-    var paintEntries = window.performance.getEntriesByName("first-paint");
-    if (paintEntries && paintEntries.length > 0) {
-      sendToSlack(`First paint is: ${paintEntries[0].startTime.toFixed(0)}ms`);
-    }
-  });
-})(window);
+// GTM
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", window.gaIdentifer.gtm);
